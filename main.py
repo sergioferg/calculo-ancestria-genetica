56f466f37c12e6e051a728ec7d48373a6e541a0b
Sergio Gomez <sergioferg.2003@gmail.com>
2026-05-30 19:15:59 -0400

    carga de csv y guardado en matriz

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
+
+datos_genotipo = []
+
+with open('datos_genotipo.csv', newline='') as csv_datos:
+    datos_reader = csv.reader(csv_datos, delimiter=',')
+    datos_genotipo = list(datos_reader)
+
+individuos = []
+
+for i, dato in enumerate(datos_genotipo):
+    if i == 0:
+        continue
+    individuos.append(dato[1:]) 
