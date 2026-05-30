70ccbd90fc9a33afc4f559b68474ec1b9b7462db
Sergio Gomez <sergioferg.2003@gmail.com>
2026-05-30 19:38:18 -0400

    frecuencias y normalizado

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
+N = 30
+M = 100
+
 datos_genotipo = []
 
 with open('datos_genotipo.csv', newline='') as csv_datos:
@@ -14,3 +17,30 @@ for i, dato in enumerate(datos_genotipo):
     if i == 0:
         continue
     individuos.append(dato[1:]) 
+
+individuos = np.array(individuos, dtype=int)
+
+frecuencias_alelo = []
+
+for j in range(0, M):
+    frecuencia = sum(individuos[:, j]) / (2*N)
+    frecuencias_alelo.append(frecuencia)
+
+frecuencias_alelo = np.array(frecuencias_alelo)
+
+normalizado_patterson = []
+
+for i in range(0, N):
+    normal = []
+    for j in range (0, M):
+        p = frecuencias_alelo[j]
+
+        if p == 0 or p == 1:
+            normal.append(0)
+        else:
+            num = individuos[i][j] - 2*frecuencias_alelo[j]
+            den = np.sqrt(2*p*(1-p))
+            normal.append(num/den)
+
+    normalizado_patterson.append(normal)
+        
